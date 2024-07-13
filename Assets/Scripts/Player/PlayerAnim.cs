524e3239399f87d91951ff6d30f0cef395f2b340
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2024-07-13 21:20:03 +0900

    Fix: player 방향전환 시점 해결

--- Assets/Scripts/Player/PlayerAnim.cs
+++ Assets/Scripts/Player/PlayerAnim.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class PlayerAnim : MonoBehaviour
 {
@@ -33,19 +34,18 @@ public class PlayerAnim : MonoBehaviour
     void Update()
     {
         angle = HarpoonHead.rotation.eulerAngles.y;
-        Debug.Log(angle);
         if (Input.GetKey(myLeft)) //왼쪽 인풋일 때
         {
             myAnimator.SetBool("isWalk", true);
-            
-            // 내가 북쪽에 있으면 왼쪽으로 걷기
-            if (angle >= 0 && angle <= 180 && PlayerIndex == 2) //p2 왼쪽으로 해주기
+
+            // 내가 남쪽에 있으면 오른쪽으로 걷기
+            if (angle >= 90 && angle <= 270 && PlayerIndex == 1) //p1 오른쪽으로 해주기
             {
                 Flip();
             }
 
-            // 내가 남쪽에 있으면 오른쪽으로 걷기
-            else if (angle > 180 && PlayerIndex == 1) //p1 오른쪽으로 해주기
+            // 내가 북쪽에 있으면 왼쪽으로 걷기
+            else if ((angle > 270 || angle < 90) && PlayerIndex == 2) //p2 왼쪽으로 해주기
             {
                 Flip();
             }
@@ -56,15 +56,15 @@ public class PlayerAnim : MonoBehaviour
         else if(Input.GetKey(myRight)) //오른쪽 인풋일 때
         {
             myAnimator.SetBool("isWalk", true);
-            // 내가 북쪽에 있으면 오른쪽으로 걷기
-            
-            if (angle >= 0 && angle <= 180 && PlayerIndex == 1) //p1 오른쪽으로 해주기
+
+            // 내가 남쪽에 있으면 왼쪽으로 걷기
+            if (angle >= 90 && angle <= 270 && PlayerIndex == 2) //p2 왼쪽으로 해주기
             {
                 Flip();
             }
 
-            // 내가 남쪽에 있으면 왼쪽으로 걷기
-            else if ((angle > 180 || angle < 0) && PlayerIndex == 2) //p2 왼쪽으로 해주기
+            // 내가 북쪽에 있으면 오른쪽으로 걷기
+            else if ((angle > 270 || angle < 90) && PlayerIndex == 1) //p1 오른쪽으로 해주기
             {
                 Flip();
             }
@@ -80,38 +80,23 @@ public class PlayerAnim : MonoBehaviour
 
     private void checkPlayerDirection()
     {
-        if(angle >= 22.5 && angle < 67.5) // BL
+        // B, BR, R, FR, F, FL, L, BL
+        int[] angles = { 0, 45, 90, 135, 180, 225, 270, 315 };
+
+        float min = 10000;
+        int min_index = 0;
+
+        for(int i=0; i<angles.Length; i++)
         {
-            mySpriteRenderer.sprite = mySprites[1];
+            float difference = Mathf.Abs(angle - angles[i]);
+            if(difference < min)
+            {
+                min = difference;
+                min_index = i;
+            }
         }
-        else if(angle >= 67.5 && angle < 112.5) // B
-        {
-            mySpriteRenderer.sprite = mySprites[2];
-        }
-        else if(angle >= 112.5 && angle < 157.5) // BR
-        {
-            mySpriteRenderer.sprite = mySprites[3];
-        }
-        else if(angle >= 157.5 && angle < 202.5) // R
-        {
-            mySpriteRenderer.sprite = mySprites[4];
-        }
-        else if(angle >= 202.5 && angle < 247.5) // FR
-        {
-            mySpriteRenderer.sprite = mySprites[5];
-        }
-        else if(angle >= 247.5 || angle < 292.5) // F
-        {
-            mySpriteRenderer.sprite = mySprites[6];
-        }
-        else if(angle >= 292.5 || angle < 337.5)// FL
-        {
-            mySpriteRenderer.sprite = mySprites[7];
-        }
-        else // L
-        {
-            mySpriteRenderer.sprite = mySprites[0];
-        }
+        mySpriteRenderer.sprite = mySprites[min_index];
+        Debug.Log(PlayerIndex +" player sprite is = "+ min_index);
     }
 
     void Flip()
