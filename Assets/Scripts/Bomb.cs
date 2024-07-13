04643c6c2fd52122d26cd2ebc7f1138a4c0024c1
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2024-07-14 03:51:13 +0900

    Feat: Bomb Sound

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -8,10 +8,16 @@ public class Bomb : MonoBehaviour
 
     Rigidbody rb;
 
+    AudioSource fuse_burn;
+    AudioSource bombhit;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.velocity = FlowManager.Flow();
+
+        fuse_burn = GameObject.Find("fuse_burn").GetComponent<AudioSource>();
+        bombhit = GameObject.Find("bombhit").GetComponent<AudioSource>();
     }
 
     void FixedUpdate()
@@ -26,6 +32,7 @@ public class Bomb : MonoBehaviour
         {
             rb.velocity = Vector3.Scale(rb.velocity, wall._reflection);
         }
+        bombhit.Play();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -35,11 +42,14 @@ public class Bomb : MonoBehaviour
             Activate();
             collision.gameObject.GetComponent<Harpoon>().Return();
         }
+        bombhit.Play();
+        fuse_burn.Stop();
     }
 
     public void Activate()
     {
         _reflect = true;
         gameObject.tag = "Untagged";
+        fuse_burn.Play();
     }
 }
